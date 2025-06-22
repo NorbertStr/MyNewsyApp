fb6011f7a2bb4230ba042c4e61578a3da3536b78
Norbert <103834660+NorbertStr@users.noreply.github.com>
2025-06-22 19:24:36 +0200

    Test Network Api with Retrofit

--- app/src/test/java/com/mynewsyapp/features_components/headline/data/remote/HeadlineApiKtTest.kt
+++ app/src/test/java/com/mynewsyapp/features_components/headline/data/remote/HeadlineApiKtTest.kt
@@ -1,17 +1,22 @@
 package com.mynewsyapp.features_components.headline.data.remote
 
+import com.google.common.truth.Truth.assertThat
 import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
 import com.mynewsyapp.MainDispatcherRule
+import com.mynewsyapp.utils.MockResponseFileReader
+import kotlinx.coroutines.test.runTest
 import kotlinx.serialization.json.Json
 import okhttp3.MediaType.Companion.toMediaType
+import okhttp3.mockwebserver.MockResponse
 import okhttp3.mockwebserver.MockWebServer
-import org.junit.Assert.*
-
+import okio.IOException
 import org.junit.After
 import org.junit.Before
 import org.junit.Rule
+import org.junit.Test
+import retrofit2.HttpException
 import retrofit2.Retrofit
-import retrofit2.create
+
 
 class HeadlineApiKtTest {
 
@@ -25,6 +30,8 @@ class HeadlineApiKtTest {
 
     @get:Rule
     val mainDispatcherRule = MainDispatcherRule()
+
+    private val succesJsonFileName = "get_article_success.json"
 
     @Before
     fun setUp() {
@@ -40,5 +47,78 @@ class HeadlineApiKtTest {
     @After
     fun tearDown() {
         mockWebServer.shutdown()
+    }
+
+    @Test
+    fun `getHeadlines should hot correct endpoint`() = runTest {
+        //GIVEN
+        mockWebServer.enqueue(MockResponse().setBody("{}").setResponseCode(200))
+        val expectedEndPoint =
+            "/v2/top-headlines?apiKey=API_KEY&category=sports&country=us&language=en&page=0&pageSize=20"
+        //WHEN
+        api.getHeadlines(
+            key = "API_KEY",
+            category = "sports",
+            country = "us",
+            language = "en",
+            pageSize = 20,
+            page = 0
+        )
+        //THEN
+        val request = mockWebServer.takeRequest()
+        assertThat(request.path).isEqualTo(expectedEndPoint)
+    }
+
+    @Test
+    fun `getHeadline should return correct data`() = runTest {
+        //Given
+        val successJson = MockResponseFileReader(succesJsonFileName).content
+        mockWebServer.enqueue(MockResponse().setBody(successJson).setResponseCode(200))
+
+        //When
+        val actualResponse = api.getHeadlines(
+            key = "API_KEY",
+            category = "sports",
+            country = "us",
+            language = "en",
+            pageSize = 20,
+            page = 0
+        )
+
+        //Then
+        assertThat(actualResponse).isNotNull()
+        assertThat(actualResponse.totalResults).isEqualTo(61)
+        assertThat(actualResponse.status).isEqualTo("ok")
+        assertThat(actualResponse.articles[0].author).isEqualTo("CNN")
+    }
+
+    @Test(expected = HttpException::class)
+    fun `getHeadline article handles 404 error`() = runTest{
+        //GIVEN
+        mockWebServer.enqueue(MockResponse().setResponseCode(404))
+        //WHEN
+        val actualResponse = api.getHeadlines(
+            key = "API_KEY",
+            category = "sports",
+            country = "us",
+            language = "en",
+            pageSize = 20,
+            page = 0
+        )
+    }
+
+    @Test(expected = IOException::class)
+    fun `getHeadline article handles network failure`() = runTest{
+        //GIVEN
+        mockWebServer.shutdown()
+        //WHEN
+        val actualResponse = api.getHeadlines(
+            key = "API_KEY",
+            category = "sports",
+            country = "us",
+            language = "en",
+            pageSize = 20,
+            page = 0
+        )
     }
 }
